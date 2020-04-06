41eb81aa5a7ba8bd8f4a8a47f66cb48bc4121107
Mahbubur Choudhury <mahbubc2012@gmail.com>
2020-04-06 18:18:45 -0400

    update

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,11 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../interfaces/todo';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
+  todos: Todo[] = [
+    {
+      task: 'Fold clothes',
+      completed: false,
+    },
+    {
+      task: 'Put clothes in dresser',
+      completed: false,
+    },
+    {
+      task: 'Relax',
+      completed: false,
+    },
+    {
+      task: 'Try to pet cat',
+      completed: true,
+    },
+    {
+      task: 'Pet dog',
+      completed: false,
+    },
+    {
+      task: 'Be Awesome',
+      completed: false,
+    },
+  ];
+  newTodo = '';
+  addTodo(newTodo) {
+    this.todos.push({
+      task: newTodo,
+      completed: false,
+    });
+    this.newTodo = '';
+  }
+  completedBtn(taskCompletedBtn) {
+    taskCompletedBtn.completed = taskCompletedBtn.completed;
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
